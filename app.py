4d5ba2867c2b86d35f4c4618f0d5c7f5712385cc
Anika Tahsin <anika.tahsin8658@gmail,com>
2023-06-27 22:12:36 +0600

    Update Database Functionality

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
-from flask import Flask, render_template, flash
+from flask import Flask, render_template, flash, request
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
 from flask_sqlalchemy import SQLAlchemy
 # from flask_mysqldb import MySQL
 from datetime import datetime
+from flask_login import UserMixin, login_user, LoginManager, login_required, logout_user, current_user
 
 
 ## Flask Instance ----------------------------------------------
@@ -12,8 +13,9 @@ app = Flask(__name__) # find all files
 
 ## Database MySQL----------------------------------------------
 # Database
+# app.config['MYSQL_HOST'] = 'localhost'
 # app.config['MYSQL_USER'] = 'rentalusers'
-# #app.config['MYSQL_PASSWORD'] = os.getenv('MYSQL_PASSWORD')
+# # #app.config['MYSQL_PASSWORD'] = os.getenv('MYSQL_PASSWORD')
 # app.config['MYSQL_PASSWORD'] = '1234'
 # app.config['MYSQL_DB'] = 'rentalusers'
 # app.config['MYSQL_CURSORCLASS'] = 'Cursor'
@@ -21,7 +23,16 @@ app = Flask(__name__) # find all files
 # app.config['SQLALCHEMY_DATABASE'] = 'rentalusers'
 
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:1234@localhost/rentalusers'
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = '1234'
+app.config['MYSQL_DB'] = 'rentalusers'
+app.config['SQLALCHEMY_DATABASE'] = 'rentalusers'
+
+
+# Default MySQL path
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:@localhost/rentalusers'
+
 # app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 ## SECRET KEY ----------------------------------------------
@@ -29,25 +40,52 @@ app.config['SECRET_KEY'] = "urgh! gotta define it" # Only for me
 
 ## Initialize Database
 db = SQLAlchemy(app)
-# db.app_context().push()
-# mysql = MySQL(app)
+# db = MySQL(app)
+app.app_context().push()
+
 
 ## Define Model ----------------------------------------------
-class Users(db.Model):
-    id = db.Column(db.Integer, primary_key = True)
+# db thingy doesnt work yet
+class Users(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key = True, unique = True)
     name = db.Column(db.String(200), nullable = False)
+    # username = db.Column(db.String(20), nullable=False)
     email = db.Column(db.String(120), nullable = False, unique = True)
     date_joined = db.Column(db.DateTime, default=datetime.utcnow)
+    # password_hash = db.Column(db.String(128))
 
-    # Creat string
+    # Create string
     def __repr__(self):
         return '<Name %r>' % self.name
-
+    
 ## Form Class ----------------------------------------------
 class Form(FlaskForm):
     name = StringField("Name: ", validators=[DataRequired()])
+    # username = StringField("Username", validators=[DataRequired()])
     email = StringField("Email: ", validators=[DataRequired()])
     submit = SubmitField("Submit")
+
+
+#=====================================================================================================================
+## Database Update ----------------------------------------------
+@app.route('/update/<int:id>', methods=['GET', 'POST'])
+def update(id):
+    form = Form()
+    name_to_update = Users.query.get_or_404(id)
+    if request.method == "POST":
+        name_to_update.name = request.form['name']
+        name_to_update.email = request.form['email']
+        try:
+            db.session.commit()
+            flash("New User Updated Successfully!!")
+            return render_template("07.update.html", form=form, name_to_update=name_to_update)
+        except:
+            db.session.commit()
+            flash("Get Permission!!")
+            return render_template("07.update.html", form=form, name_to_update=name_to_update)
+    else:
+        return render_template("07.update.html", form=form, name_to_update=name_to_update)
+    
 
 ## ADD User ----------------------------------------------
 @app.route('/user/add', methods=['GET', 'POST'])
@@ -55,18 +93,20 @@ def add_user():
     name = None
     form = Form()
     if form.validate_on_submit():
+        # db thingy doesnt work yet
         user = Users.query.filter_by(email=form.email.data).first()
-        if user in None:
-            user = Users(name=form.name.data, email=form.email.data)
+        if user is None:
+            user = Users(name=form.name.data, email=form.email.data) #, username=form.username.data
             db.session.add(user)
             db.session.commit()
         name = form.name.data
         form.name.data = ''
+        # form.username.data = ''
         form.email.data = ''
         flash("New User Added Successfully!!")
-    here_users = Users.query.order_by(Users.date_joined)
+    here_users = Users.query.order_by(Users.date_joined) # db thingy doesnt work yet
     return render_template("05.add_user.html", form=form, name=name, here_users=here_users)
-
+    # return render_template("05.add_user.html", form=form, name=name)
 
 ## Creating route (url) decorator ----------------------------------------------
 @app.route('/') # root route, main
@@ -74,7 +114,9 @@ def add_user():
 def index():
     name_test = "Bleh"
     shop = ['coffee', 'fried chicken', 'momo']
+    
     return render_template("01.index.html", name_test=name_test, shopping = shop)
+    # return render_template("01.index.html")
 
 # if __name__ == '__main__': app.run(debug=True)
 
--- create_db.py
+++ create_db.py
@@ -1,15 +1,15 @@
 import mysql.connector
 
 data = mysql.connector.connect(
-    host = 'localhost',
-    user = 'root',
-    passwd = '1234',
-    )
+	host="localhost",
+	user="root",
+	# passwd = "1234", #gives error
+	)
 
-cursor = data.cursor() # automated database commands
+cursor = data.cursor()
 
-cursor.execute("CREATE DATABASE rentaluser")
+# cursor.execute("CREATE DATABASE rentalusers")
+
 cursor.execute("SHOW DATABASES")
-
 for db in cursor:
-    print(db)
+	print(db)
