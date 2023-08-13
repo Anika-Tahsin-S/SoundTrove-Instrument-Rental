0f1f1b675126645578e3d622b9324b6794afdce8
Anika Tahsin <anika.tahsin8658@gmail,com>
2023-08-13 17:06:45 +0600

    DropDown Feature Included

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -51,26 +51,34 @@
                                 </li>
                             @endif
                         @else
-                            <li class="nav-item dropdown">
-                                <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                                    {{ Auth::user()->name }}
-                                </a>
-                                <ul class="dropdown-menu">
-                                    <li>
-                                        <a class="dropdown-item" href="{{ route('admin-user') }}">
-                                            My Profile
-                                        </a>
-                                    </li>
-                                    <li>
-                                        <a class="dropdown-item" href="{{ route('logout') }}"  onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
-                                            {{ __('Logout') }}
-                                        </a>
-                                        <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                                            @csrf
-                                        </form>
-                                    </li>
-                                </ul>
-                            </li>
+                        <li class="nav-item dropdown">
+                            <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                {{ Auth::user()->name }}
+                            </a>
+
+                        <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
+                            <a class="dropdown-item" href="{{ url('admin-user') }}">
+                                My Profile
+                            </a>
+                            <a class="dropdown-item" href="{{ url('my-orders') }}">
+                                My Rents
+                            </a>
+                            <a class="dropdown-item" href="{{ url('cart') }}">
+                                My Cart
+                            </a>
+                            <a class="dropdown-item" href="{{ url('wishlist') }}">
+                                My Wishlist
+                            </a>
+                            <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault();
+                                                        document.getElementById('logout-form').submit();">
+                                {{ __('Logout') }}
+                            </a>
+
+                            <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                                @csrf
+                            </form>
+                        </div>
+                    </li>
                         @endguest
                     </ul>
                 </div>
